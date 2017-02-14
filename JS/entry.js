e9af86eafffa66a43bfbf73f415ce350d2a8289c
Mac <taralgurung@hotmail.com>
2017-02-14 11:57:58 +0800

    2nd commit

--- JS/entry.js
+++ JS/entry.js
@@ -11,7 +11,9 @@ Entry.prototype.vowels = function(bodyEntry) {
   bodyEntry = bodyEntry.toLowerCase();
   var vowelsCount = 0;
   for (var i = 0; i <= bodyEntry.length; i += 1) {
-    if (bodyEntry.charAt(i) == "a" || bodyEntry.charAt(i) == "e" || bodyEntry.charAt(i) == "i" || bodyEntry.charAt(i) == "o" || bodyEntry.charAt(i) == "u") {
+    if (bodyEntry.charAt(i) == "a" ||
+    bodyEntry.charAt(i) == "e" ||
+    bodyEntry.charAt(i) == "i" || bodyEntry.charAt(i) == "o" || bodyEntry.charAt(i) == "u") {
       vowelsCount += 1;
     }
   }
@@ -22,7 +24,8 @@ Entry.prototype.consonants = function(bodyEntry) {
   bodyEntry = bodyEntry.toLowerCase();
   var consonantsCount = 0;
   for (var i = 0; i <= bodyEntry.length; i += 1) {
-    if (bodyEntry.charAt(i) == "a" || bodyEntry.charAt(i) == "e" || bodyEntry.charAt(i) == "i" || bodyEntry.charAt(i) == "o" || bodyEntry.charAt(i) == "u") {
+    if (bodyEntry.charAt(i) == "a" || bodyEntry.charAt(i) == "e"
+    || bodyEntry.charAt(i) == "i" || bodyEntry.charAt(i) == "o" || bodyEntry.charAt(i) == "u") {
       consonantsCount += 0;
     } else {
       consonantsCount += 1;
@@ -31,7 +34,7 @@ Entry.prototype.consonants = function(bodyEntry) {
   return consonantsCount;
 }
 
-// exports.entryModule = Entry;
+//exports.entryModule = Entry;
 
 $(document).ready(function() {
   $('#entry-form').submit(function(event) {
